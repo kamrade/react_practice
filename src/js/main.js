a265ecc090afd29b985c5a42f8dfad8ed7285489
Denis Michailov <kamrad.reg@gmail.com>
2016-08-06 00:39:14 +1000

    My first component

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,20 @@ console.log( "D3 version: " + d3.version );
 console.log("Test");
 
 
+var React = require('react');
+var ReactDOM = require('react-dom')
+
+// Store picker component
+// This will let us make <StorePicker/>
+var StorePicker = React.createClass({
+
+	render: function(){
+		return (
+			<p>hello</p>
+		)
+	}
+
+});
+
+
+ReactDOM.render(<StorePicker/>, document.querySelector("#main"));
