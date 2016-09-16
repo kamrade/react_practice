12d4379392a0402064247aac269b19648f2ef4ec
Denis Michailov <kamrad.reg@gmail.com>
2016-09-16 22:56:17 +1000

    basic component with prop

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,19 @@
 console.log( "D3 version: " + d3.version );
-console.log("Test");
-
 
 var React = require('react');
 var ReactDOM = require('react-dom')
 
-// Store picker component
-// This will let us make <StorePicker/>
 var Component = React.createClass({
 
 	render: function(){
 		return (
 			<div className="component">
 				<h1>This is my component</h1>
-				<h3>And small description to it</h3>
-				<p>Nullam id dolor id nibh ultricies vehicula ut id elit. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Curabitur blandit tempus porttitor. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper.</p>
+				<h3>Hello, {this.props.name}</h3>
 			</div>
 		)
 	}
 
 });
 
-
-ReactDOM.render(<Component/>, document.querySelector("#main"));
+ReactDOM.render(<Component name="Denis"/>, document.querySelector("#main"));
