481919662878ae809f93f2f8ba1c83c5d314be32
Denis Michailov <kamrad.reg@gmail.com>
2016-09-16 23:45:49 +1000

    events

--- src/js/main.js
+++ src/js/main.js
@@ -6,16 +6,25 @@ let ReactDOM = require('react-dom')
 
 
 let Jumbotron = React.createClass({
+	getDefaultProps : function() {
+		return {
+			title: "Hello Title!",
+			text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ea ducimus natus labore velit aspernatur sunt ipsum explicabo libero, temporibus suscipit, cumque, quos accusantium. Corrupti tempora maxime, iure molestias recusandae."
+		};
+	},
 	render: function(){
 		return(
 			<div>
 				<div className="jumbotron">
-					<h1>Title</h1>
-					<p>...</p>
-					<p><a href="#" className="btn btn-primary btn-lg" role="button">Learn More</a></p>
+					<h1>{this.props.title}</h1>
+					<p>{this.props.text}</p>
+					<p><a onClick={this.onClick.bind(this, 'Racoon', 'Fox')} href="#" className="btn btn-primary btn-lg" role="button">Learn More</a></p>
 				</div>
 			</div>
 		);
+	},
+	onClick: function(beast1, beast2){
+		console.log("click " + beast1 + " | " + beast2);
 	}
 });
 
