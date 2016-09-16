0d55e651db5a0827fbfcd127b3b80fc1780ef106
Denis Michailov <kamrad.reg@gmail.com>
2016-09-16 23:35:12 +1000

    jumbotron component

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,25 @@
-console.log( "D3 version: " + d3.version );
 
-var React = require('react');
-var ReactDOM = require('react-dom')
+let React = require('react');
+let ReactDOM = require('react-dom')
 
 
-// getInitialState
-// getDefaultProps
-// componentWillMount
-// componentDidMount
 
-var Jumbotron = React.createClass({
-	propTypes: {
-		// свойство title должно быть строкой и оно required
-		title: React.PropTypes.string.isRequired
-	},
-	getDefaultProps: function(){
-		return {
-			title      : "Hey, this is the title",
-			main_title : "Hello woo!",
-			link       : "http://google.com"
-		};
-	},
+
+let Jumbotron = React.createClass({
 	render: function(){
 		return(
 			<div>
 				<div className="jumbotron">
-					<h1>{this.props.main_title}</h1>
-					<a href={this.props.link}><p>{this.props.title}</p></a>
+					<h1>Title</h1>
+					<p>...</p>
+					<p><a href="#" className="btn btn-primary btn-lg" role="button">Learn More</a></p>
 				</div>
 			</div>
 		);
 	}
-})
-
-var Component = React.createClass({
-	render: function(){
-		return (
-			<div className="component">
-				<strong><p>This is my component, {this.props.name}</p></strong>
-			</div>
-		)
-	}
 });
 
+
+
+
 ReactDOM.render(<Jumbotron />, document.querySelector("#jumbotron"));
-ReactDOM.render(<Component name="Denis"/>, document.querySelector("#main"));
